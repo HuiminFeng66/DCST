16efb86cd37cdbe3ce46f3f2a80360804c93a2b2
HuiminFeng66 <75473625+HuiminFeng66@users.noreply.github.com>
2023-02-21 15:33:39 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#sudo apt update
+#sudo apt install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt update
+#sudo apt -y install python3
+#sudo apt-get --purge autoremove python3-pip
+#sudo apt install python3-pip
+pip3 install matplotlib
+pip3 install luminol
+pip3 install sklearn
+pip3 install numpy
+pip3 install pandas
+pip3 install tqdm
+pip3 install sklearn
+pip3 install plotly
